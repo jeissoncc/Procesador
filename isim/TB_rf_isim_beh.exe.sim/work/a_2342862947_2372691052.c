6e60547666670e7eccc50ab0413cb164d369fedb
jeisson <jcastiblanco@utp.edu.co>
2017-10-02 23:41:34 -0500

    Procesador 1

---
+++ isim/TB_rf_isim_beh.exe.sim/work/a_2342862947_2372691052.c
@@ -0,0 +1,91 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                       */
+/*  \   \        Copyright (c) 2003-2009 Xilinx, Inc.                */
+/*  /   /          All Right Reserved.                                 */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                      */
+/*  \___\/\___\                                                    */
+/***********************************************************************/
+
+/* This file is designed for use with ISim build 0x8ef4fb42 */
+
+#define XSI_HIDE_SYMBOL_SPEC true
+#include "xsi.h"
+#include <memory.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+static const char *ng0 = "C:/Documents and Settings/Administrador/Mis documentos/Arquitectura/Procesador/TB_rf.vhd";
+
+
+
+static void work_a_2342862947_2372691052_p_0(char *t0)
+{
+    char *t1;
+    char *t2;
+    char *t4;
+    char *t5;
+    char *t6;
+    char *t7;
+    char *t8;
+
+LAB0:    t1 = (t0 + 1712U);
+    t2 = *((char **)t1);
+    if (t2 == 0)
+        goto LAB2;
+
+LAB3:    goto *t2;
+
+LAB2:    xsi_set_current_line(82, ng0);
+    t2 = (t0 + 4059);
+    t4 = (t0 + 1944);
+    t5 = (t4 + 32U);
+    t6 = *((char **)t5);
+    t7 = (t6 + 40U);
+    t8 = *((char **)t7);
+    memcpy(t8, t2, 5U);
+    xsi_driver_first_trans_fast(t4);
+    xsi_set_current_line(83, ng0);
+    t2 = (t0 + 4064);
+    t4 = (t0 + 1980);
+    t5 = (t4 + 32U);
+    t6 = *((char **)t5);
+    t7 = (t6 + 40U);
+    t8 = *((char **)t7);
+    memcpy(t8, t2, 5U);
+    xsi_driver_first_trans_fast(t4);
+    xsi_set_current_line(84, ng0);
+    t2 = (t0 + 4069);
+    t4 = (t0 + 2016);
+    t5 = (t4 + 32U);
+    t6 = *((char **)t5);
+    t7 = (t6 + 40U);
+    t8 = *((char **)t7);
+    memcpy(t8, t2, 5U);
+    xsi_driver_first_trans_fast(t4);
+    xsi_set_current_line(85, ng0);
+
+LAB6:    *((char **)t1) = &&LAB7;
+
+LAB1:    return;
+LAB4:    goto LAB2;
+
+LAB5:    goto LAB4;
+
+LAB7:    goto LAB5;
+
+}
+
+
+extern void work_a_2342862947_2372691052_init()
+{
+	static char *pe[] = {(void *)work_a_2342862947_2372691052_p_0};
+	xsi_register_didat("work_a_2342862947_2372691052", "isim/TB_rf_isim_beh.exe.sim/work/a_2342862947_2372691052.didat");
+	xsi_register_executes(pe);
+}
